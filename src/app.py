482bfc92e6b0a91b5f24b38d951286b5d7982fec
Jacob-Mowat <zaxquit@protonmail.com>
2019-09-14 19:37:52 +0100

    Added /blocks/<id> route'

--- src/app.py
+++ src/app.py
@@ -8,6 +8,8 @@ import re
 
 from flask import Flask, render_template
 
+from . import backend as BE
+
 BLOCKS_DIR='/Users/jacob/Desktop/boat_bored/data_store/blocks/'
 
 app = Flask(__name__)
@@ -27,4 +29,10 @@ def display_blocks():
             )
     
     return render_template('blocks.html', blocks=blocks)
+
+@app.route('/blocks/<block_id>')
+def display_block(block_id: int):
+    block = BE.get_block(block_id)
+
+    return render_template('block_info.html', block=block)
 
